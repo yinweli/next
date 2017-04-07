0ea0fd4942c6ad9c35cf714e70c4a6ce769b9eb4
yinweli <yinweli@yahoo.com.tw>
2017-04-07 15:54:59 +0800

    新增unity插件庫

---
+++ next/next.unity.addon/Singleton/Singleton.cs
@@ -0,0 +1,16 @@
+﻿// 繼承此類別後, 便擁有Singleton設計模式的能力
+public class Singleton<T> where T : class, new()
+{
+    private static T instance = null;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new T();
+
+            return instance;
+        }
+    }
+}
